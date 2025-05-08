a7c50b237e68a6908806ac37cf598bbb6c10c91d
MRamazan <mramazantrn28@gmail.com>
2025-05-08 20:23:53 +0300

    Update Alexnet.py

--- Alexnet.py
+++ Alexnet.py
@@ -5,13 +5,13 @@ from torch import nn
 class AlexNet(nn.Module):
     def __init__(self, num_classes=100):
         super(AlexNet, self).__init__()
-        self.Conv1 = nn.Conv2d(3, 96, 11,4,2)
-        self.Conv2 = nn.Conv2d(96,256,5,1,2)
-        self.Conv3 = nn.Conv2d(256,384,3,1,1)
-        self.Conv4 = nn.Conv2d(384,384,3,1,1)
+        self.Conv1 = nn.Conv2d(3, 96, 11, 4, 2)
+        self.Conv2 = nn.Conv2d(96, 256, 5, 1, 2)
+        self.Conv3 = nn.Conv2d(256, 384, 3, 1, 1)
+        self.Conv4 = nn.Conv2d(384, 384, 3, 1, 1)
         self.Conv5 = nn.Conv2d(384, 256, 3, 1, 1)
 
-        self.LocalResponseNorm = nn.LocalResponseNorm(5, beta=0.75,alpha=0.0001,k=2)
+        self.LocalResponseNorm = nn.LocalResponseNorm(5, beta=0.75, alpha=0.0001, k=2)
         self.ReLu = nn.ReLU()
         self.MaxPool = nn.MaxPool2d(3,2)
 
